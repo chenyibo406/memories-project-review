c3ad2a708bb1c2e5df4b0019c97cca881fda851c
Austin Chen <63479915+chenyibo406@users.noreply.github.com>
2023-02-11 17:49:35 +0800

    Update auth.js

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,10 +2,10 @@ import * as api from "../api";
 import { AUTH } from "../constants/actionTypes";
 
 export const signin = (formData, navigate) => async (dispatch) => {
-  // const dispatch = useDispatch();
+ 
   try {
     const { data } = await api.signIn(formData);
-    // console.log(data);
+    
     dispatch({ type: AUTH, data });
     navigate("/");
   } catch (error) {
@@ -14,7 +14,7 @@ export const signin = (formData, navigate) => async (dispatch) => {
 };
 export const signup = (formData, navigate) => async (dispatch) => {
   try {
-    // sign up the user...
+   
     const { data } = await api.signUp(formData);
     dispatch({ type: AUTH, data });
     navigate("/");
@@ -23,29 +23,4 @@ export const signup = (formData, navigate) => async (dispatch) => {
   }
 };
 
-// import { AUTH } from "../constants/actionTypes";
-// import * as api from "../api/index.js";
 
-// export const signin = (formData, router) => async (dispatch) => {
-//   try {
-//     const { data } = await api.signIn(formData);
-
-//     dispatch({ type: AUTH, data });
-
-//     router.push("/");
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// export const signup = (formData, router) => async (dispatch) => {
-//   try {
-//     const { data } = await api.signUp(formData);
-
-//     dispatch({ type: AUTH, data });
-
-//     router.push("/");
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
